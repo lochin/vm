06b33a345eb22acf3023c4d1ec39e9665e5d1333
YusupovJobir <jobiryusupov0@gmail.com>
2019-03-11 00:40:04 +0500

    admin panel col

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -8,13 +8,21 @@
             <li class="list-group-item list-group-item-action my-2">
                 <div class="row">
                 <div class="md-v-line"></div>
-                    <div class="col-sm-5">
-                        <b>{{ $applies->total() - ($key+($applies->currentPage()-1)*$applies->perPage()) }}. </b>
-                        <i class="{{ ($apply->status == 1) ? 'fab fa-readme text-success' : 'fas fa-book text-danger'}} mr-4 pr-3"></i>
-                         <span>{{ $apply->firstname.' '.$apply->lastname }}</span>
+                    <div class="col-sm-2">
+                        <span class="text-nowrap">
+                            <b>{{ $applies->total() - ($key+($applies->currentPage()-1)*$applies->perPage()) }}. </b>
+                            <i class="{{ ($apply->status == 1) ? 'fab fa-readme text-success' : 'fas fa-book text-danger'}} mr-4 pr-3"></i>
+                        </span>
                     </div>
-                    <div class="col-sm-7">
-                        <span>{{ $apply->region.' '.$apply->district }}</span>
+                    <div class="col">
+                        <div class="row">
+                            <div class="col">
+                                <span>{{ $apply->firstname.' '.$apply->lastname }}</span>
+                            </div>
+                            <div class="col-sm-7">
+                                <span>{{ $apply->region.' '.$apply->district }}</span>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </li>
