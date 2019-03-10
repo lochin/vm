5132edd738f9b2e8be9c6df77d6e2054353d7ce4
YusupovJobir <jobiryusupov0@gmail.com>
2019-03-11 00:09:14 +0500

    header test. admin panel chala

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -3,21 +3,23 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+        <ul class="list-group w-100">
+            @foreach($applies as $apply)
+            <li class="list-group-item list-group-item-action my-2">
+                <div class="row">
+                <div class="md-v-line"></div>
+                    <div class="col-sm-5">
+                        <i class="{{ ($apply->status == 1) ? 'fab fa-readme text-success' : 'fas fa-book text-danger'}} mr-4 pr-3"></i>
+                         <span>{{ $apply->firstname.' '.$apply->lastname }}</span>
+                    </div>
+                    <div class="col-sm-7">
+                        <span>{{ $apply->region.' '.$apply->district }}</span>
+                    </div>
                 </div>
-            </div>
-        </div>
+            </li>
+            @endforeach
+        </ul>
+        {{ $applies->links() }}
     </div>
 </div>
 @endsection
